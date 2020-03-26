b5c6ec9f320b012783676b0d11665368585c632a
gaspar.110992 <gaspar.110992@e.unavarra.es>
2020-03-26 16:48:49 +0000

    proyecto android

---
+++ p02/ActivityA2020/app/src/main/java/com/example/activitya2020/ActivityA110992.java
@@ -0,0 +1,14 @@
+package com.example.activitya2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityA110992 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
